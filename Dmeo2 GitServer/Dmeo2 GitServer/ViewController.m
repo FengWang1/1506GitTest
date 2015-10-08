03bc4303c9a62d98bd148aa6626a037ae8fd6007
tarena <tarena@fengboAc.local>
2015-10-08 11:55:56 +0800

    111

---
+++ Dmeo2 GitServer/Dmeo2 GitServer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Dmeo2 GitServer
+//
+//  Created by tarena on 15/10/8.
+//  Copyright (c) 2015年 Ac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Dmeo2 GitServer/Dmeo2 GitServer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dmeo2 GitServer
+//
+//  Created by tarena on 15/10/8.
+//  Copyright (c) 2015年 Ac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
